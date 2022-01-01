93c03b41f5f3d7d369933839ebea1f81de6fb94d
rusty-reebs <rusty_reebs@tutanota.com>
2022-01-01 11:36:39 +0400

    write schemas for database

---
+++ models/bookinstance.js
@@ -0,0 +1,23 @@
+// bookinstance.js
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    require: true,
+    enum: ["Available", "Maintenance", "Loaned", "Reserved"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/bookinstance/" + this._id;
+});
+
+module.exports = mongoose.model("BookInstance", BookInstanceSchema);
